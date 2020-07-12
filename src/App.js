067518d2d41b182487c4b71d87bc7b6ecd1d093c
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2020-07-12 15:58:36 +0530

    added wind

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ function App() {
           <div className="weather-box">
             <div className="temp">{Math.round(weather.main.temp)}°c</div>
             <div className="weather">{weather.weather[0].main}</div>
+            <div className="wind">Wind: {weather.wind.speed} kmph</div>
           </div>
         </div>  
         
